a5f28bf5b5b5b66b679e4ecfa440eb17efeeff0e
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2022-06-07 21:47:59 -0300

    fix: fixing duplicate import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [UsersModule, AuthModule],
